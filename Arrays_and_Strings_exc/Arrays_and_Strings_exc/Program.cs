1d58eaba4b932eee046598eeff6cd3a8c410abc5
SanFabryino <31211801+SanFabryino@users.noreply.github.com>
2019-03-06 21:19:54 +0100

    nuovi proggetti

---
+++ Arrays_and_Strings_exc/Arrays_and_Strings_exc/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_and_Strings_exc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //----------------------------------------------------------------------------------------------------------------------------
+            /*int[] myArray = new int[10];
+            for (int k = 0; k < 10; k++)
+            {
+                myArray[k] = k * 2;                 //stampo le tabelline (foreach loop)
+            }
+
+            foreach (var k in myArray)
+            {
+                Console.WriteLine(k);
+            }
+
+            Console.ReadKey();
+            */
+            //----------------------------------------------------------------------------------------------------------------------------
+            /*int[] arr = { 12, 15, 28 };
+            for (int k = 0; k < arr.Length; k++)
+            {
+                Console.WriteLine(arr[k]);                  //arr.Length 
+            }
+            Console.ReadKey();
+            */
+            //----------------------------------------------------------------------------------------------------------------------------
+            /*int[] arr = { 2, 7, 1, 5, 9 };
+
+            Console.WriteLine(arr.Sum());                           //arr.Max/Min/Sum
+
+            Console.ReadKey();
+            */
+            //----------------------------------------------------------------------------------------------------------------------------
+            /*string a = "some text";
+            Console.WriteLine(a.IndexOf('t'));                            //a.Length/IndexOf/Insert/Replace/Contains/Remove/Substring
+            Console.ReadKey();
+            */
+            //----------------------------------------------------------------------------------------------------------------------------
+            /*string text = "questo è del testo riguardante un bel cane di nome 'cane', questo cane è moto bello e molto giocherellone, proprio come un vero cane";
+
+            text = text.Replace("cane", "gatto");
+                                                                                                                                                                         //text replacer
+            Console.WriteLine(text);
+
+            Console.ReadKey();
+            */
+            //----------------------------------------------------------------------------------------------------------------------------
+            /*string[] names = new string[2] { "Fabrizio", "Gianluca"};
+            Console.WriteLine("Come ti chiami?");
+            names[1] = Convert.ToString(Console.ReadLine());                        //queste sono strings
+            Console.WriteLine("Ciao{0} ", names[1]);
+            
+            Console.ReadKey();*/
+        }
+    }
+}
---
+++ Calcolatore_Valori_Nutrizionali/Calcolatore_Valori_Nutrizionali/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcolatore_Valori_Nutrizionali
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double calorie, carboidrati, proteine,grassi, quant;
+            calorie = 0;
+            carboidrati = 0;
+            proteine = 0;
+            grassi = 0;
+            quant = 0;
+            //dialoghi--------------------------------------------------------------------------
+            Console.WriteLine("Ciao, benvenuto nel calcolatore dei valori nutrizionali...");
+
+            Console.WriteLine("Nel tuo cibo...quante calorie ci sono ogni 100 g?");
+            calorie = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("quanti carboidrati ogni 100 g?");
+            carboidrati = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("quante proteine ogni 100 g?");
+            proteine = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("quanti grassi ogni 100 g?");
+            grassi = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Adesso...quanti grammi vuoi mangiare di questo cibo??");
+            quant = Convert.ToDouble(Console.ReadLine());
+            //calcoli----------------------------------------------------------------------
+
+            calorie = calorie * quant;
+            calorie = calorie / 100;
+
+            carboidrati = carboidrati * quant;
+            carboidrati = carboidrati / 100;
+
+            proteine = proteine * quant;
+            proteine = proteine / 100;
+
+            grassi = grassi * quant;
+            grassi = grassi / 100;
+            //risultati---------------------------------------------------------------------
+            Console.WriteLine("Elaborazione...Nel tuo cibo adesso ci sono esattamente:");
+
+            Console.WriteLine("Calorie:{0}", calorie);
+
+            Console.WriteLine("Carboidrati:{0}", carboidrati);
+
+            Console.WriteLine("Proteine:{0}", proteine);
+
+            Console.WriteLine("Grassi:{0}", grassi);
+            //END----------------------------------------------------------------------------
+            Console.ReadKey();
+        }
+    }
+}
